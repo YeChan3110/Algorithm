bf77c05c0d732aff367d0e8d91fa8b382d863d9a
yechan <yechan3110@gmail.com>
2023-03-31 23:50:58 +0900

    commit

---
+++ Programmers/최빈값구하기/Solution.java
@@ -0,0 +1,14 @@
+package Programmers.최빈값구하기;
+
+public class Solution {
+    public int solution(int[] array) {
+        int answer = 0;
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Solution sl = new Solution();
+        int[] arr = {1,2,2};
+        System.out.println(arr.length);
+    }
+}
