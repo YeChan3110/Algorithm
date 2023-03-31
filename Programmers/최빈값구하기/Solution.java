b965b58ee6fb5d38bbb165e1d02ed1a3a095c9c6
yechan <yechan3110@gmail.com>
2023-04-01 01:24:59 +0900

    commit

--- Programmers/최빈값구하기/Solution.java
+++ Programmers/최빈값구하기/Solution.java
@@ -8,29 +8,19 @@ import java.util.Map;
 
 public class Solution {
     public int solution(int[] array) {
-        int answer = 0;
-        return answer;
-    }
-
-    public static void main(String[] args) {
         Map<Integer,Integer> map = new HashMap<>();
 
-        int[] arr = {1,2,3,4,5,6,8,8,7,7};
-
-        for(int i = 0; i < arr.length; i++){
-            if(map.get(arr[i]) == null){
-                map.put(arr[i],1);
+        for(int i = 0; i < array.length; i++){
+            if(map.get(array[i]) == null){
+                map.put(array[i],1);
             }else{
-                map.put(arr[i],map.get(arr[i])+1);
+                map.put(array[i],map.get(array[i])+1);
             }
         }
-        System.out.println(map);
 
-        // 최대 횟수
         int max = Collections.max(map.values());
         
         List<Integer> maxKeys = new ArrayList<>();
-
         for(Map.Entry<Integer,Integer> entry : map.entrySet()){
             if (entry.getValue() == max) {
                 maxKeys.add(entry.getKey());
@@ -38,7 +28,11 @@ public class Solution {
         }
 
         if(maxKeys.size() > 1){
-            System.out.println("큼");
+            return -1;
+        }else{
+            return maxKeys.get(0);
         }
     }
+
+
 }
