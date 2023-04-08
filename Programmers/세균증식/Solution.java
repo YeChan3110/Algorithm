abb8b112654435d8157bba749bd6c1700ee3218f
yechan <yechan3110@gmail.com>
2023-04-08 22:48:21 +0900

    commit

---
+++ Programmers/세균증식/Solution.java
@@ -0,0 +1,15 @@
+package Programmers.세균증식;
+
+public class Solution {
+    public int solution(int n, int t){
+        int answer = n;
+        for(int i = 0; i < t; i++){
+            answer *= 2;
+        }
+        return answer;
+    }
+    public static void main(String[] args) {
+        System.out.println(new Solution().solution(7, 15));
+    }
+
+}
