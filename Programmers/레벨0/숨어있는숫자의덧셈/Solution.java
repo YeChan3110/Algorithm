74c5366c29fb110ef2b5efa14477977c63cab04a
yechan <yechan3110@gmail.com>
2023-04-16 22:08:44 +0900

    commit

--- Programmers/레벨0/숨어있는숫자의덧셈/Solution.java
+++ Programmers/레벨0/숨어있는숫자의덧셈/Solution.java
@@ -1,27 +1,19 @@
 package Programmers.레벨0.숨어있는숫자의덧셈;
 
-import java.util.ArrayList;
+import java.util.regex.*;
 
 public class Solution {
     public int solution(String my_string) {
         int answer = 0;
-        String regex = "[0-9]";
-        ArrayList<Integer> arr = new ArrayList<>();
 
-        for(char c : my_string.toCharArray()){
-            if(String.valueOf(c).matches(regex)){
-                arr.add(my_string.indexOf(c));
-            }
+        Pattern pattern = Pattern.compile("\\d+");
+        Matcher matcher = pattern.matcher(my_string);
+
+        while (matcher.find()) {
+            String match = matcher.group();
+            answer += Integer.parseInt(match);
         }
-
-        for(int i = 0; i < arr.size(); i++){
-            if(arr.get(i+1) - arr.get(i) != 1){
-                answer += Integer.parseInt(String.valueOf(my_string.charAt(arr.get(i))));
-            }
-        }
-
         System.out.println(answer);
-
         return answer;
     }
     public static void main(String[] args) {
