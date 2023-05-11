09597114faa955c24e31c4026302b9c557280f95
Yechan <115390448+YeChan3110@users.noreply.github.com>
2023-05-11 20:05:30 +0900

    Update Solution.java

--- Programmers/레벨1/x만큼간격이있는n개의숫자/Solution.java
+++ Programmers/레벨1/x만큼간격이있는n개의숫자/Solution.java
@@ -4,10 +4,12 @@ public class Solution {
     public long[] solution(int x, int n) {
         long[] answer = new long[n];
         long a = 0;
+        
         for(int i = 0; i < n; i++){
             answer[i] += x+a;
             a += x;
         }
+        
         return answer;
     }
 }
