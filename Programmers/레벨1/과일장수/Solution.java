8120d45afd864a2e88d793521e6a0ddf902c76b3
yechan <yechan3110@gmail.com>
2023-06-20 23:06:19 +0900

    commit

---
+++ Programmers/레벨1/과일장수/Solution.java
@@ -0,0 +1,39 @@
+package Programmers.레벨1.과일장수;
+
+import java.util.Arrays;
+
+public class Solution {
+    public static int minOfString(String s){
+        String[] num = s.split(" ");
+        int min = Integer.valueOf(num[0]);
+        for(int i=1;i<num.length;i++){
+            if(Integer.valueOf(num[i]) < min) min = Integer.valueOf(num[i]);
+        }
+        return min;
+    }
+    //배열 뒤집기
+    public static void reverseArr(int[] arr){
+        int length = arr.length;
+        for(int i=0;i<length/2;i++){
+            int temp = arr[length -1 -i];
+            arr[length -1 -i] = arr[i];
+            arr[i] = temp;
+        }
+    }
+
+    public int solution(int k, int m, int[] score) {
+        int answer = 0;
+        Arrays.sort(score);
+        reverseArr(score);
+        StringBuilder sb = new StringBuilder();
+        for(int i=0; i < score.length - m + 1; i += m) {
+            for(int j=i;j<i+m;j++){
+                sb.append(score[j]+" ");
+            }
+            answer += minOfString(sb.toString()) * m;
+            sb.setLength(0);
+        }
+
+        return answer;
+    }
+}
