7e9951313a6a6d541cd27286a9e1951d5ce33053
yechan <yechan3110@gmail.com>
2023-07-13 23:56:48 +0900

    commit

--- Programmers/레벨2/JadenCase문자열만들기/Solution.java
+++ Programmers/레벨2/JadenCase문자열만들기/Solution.java
@@ -3,6 +3,13 @@ package Programmers.레벨2.JadenCase문자열만들기;
 public class Solution {
     public String solution(String s) {
         String answer = "";
+        String[] str = s.toLowerCase().split("");
+        boolean space = true;
+
+        for(String st : str) {
+            answer += space ? st.toUpperCase() : st;
+            space = st.equals(" ") ? true : false;
+        }
         return answer;
     }
 }
