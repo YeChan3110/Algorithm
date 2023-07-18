89141a9496afdc959d4442469272a2ef36d7b997
yechan <yechan3110@gmail.com>
2023-07-18 23:18:59 +0900

    commit

--- Programmers/레벨2/최솟값만들기/Solution.java
+++ Programmers/레벨2/최솟값만들기/Solution.java
@@ -1,10 +1,18 @@
 package Programmers.레벨2.최솟값만들기;
 
+import java.util.Arrays;
+
 public class Solution {
     public int solution(int []A, int []B)
     {
         int answer = 0;
+        Arrays.sort(A);
+        Arrays.sort(B);
 
+        for(int i = 0; i < A.length; i++){
+            answer = answer + (A[i] * B[A.length - i - 1]);
+        }
+        
         return answer;
     }
 }
