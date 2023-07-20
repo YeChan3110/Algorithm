21e9aeb9d250d2f1c24dd507a96dd9001b32f29d
yechan <yechan3110@gmail.com>
2023-07-20 22:39:55 +0900

    commit

--- Programmers/레벨2/숫자의표현/Solution.java
+++ Programmers/레벨2/숫자의표현/Solution.java
@@ -2,9 +2,20 @@ package Programmers.레벨2.숫자의표현;
 
 public class Solution {
     public int solution(int n) {
-        int answer = 0;
+        if(n == 1 || n == 2) return 1;
 
+        int answer = 0;
         
+        for(int i = 0; i < n; i++){
+            int temp = 0;
+            for(int j = 0; j < n; j++){
+                if(temp == 15){
+                    answer ++;
+                    break;
+                }
+                temp += j;
+            }
+        }
 
         return answer;
     }
