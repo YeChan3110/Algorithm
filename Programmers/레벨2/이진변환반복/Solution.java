785159cb3d956e269d840192c94906379c7477c0
yechan <yechan3110@gmail.com>
2023-07-26 00:41:16 +0900

    commit

--- Programmers/레벨2/이진변환반복/Solution.java
+++ Programmers/레벨2/이진변환반복/Solution.java
@@ -1,8 +1,35 @@
 package Programmers.레벨2.이진변환반복;
 
 public class Solution {
+    static int count = 0;
+    static int zero = 0;
+
     public int[] solution(String s) {
         int[] answer = {};
+        System.out.println(del(s));
+        System.out.println(count);
+        System.out.println(zero);
         return answer;
+    }
+
+    public static String del(String s){
+
+        for(char c : s.toCharArray()){
+            if(c == '0'){
+                zero++;
+            }
+        }
+
+        if(s.equals("10")){
+            count++;
+            zero++;
+            return "1";
+        }
+
+        return s;
+    }
+
+    public static void main(String[] args) {
+        new Solution().solution("1010");
     }
 }
