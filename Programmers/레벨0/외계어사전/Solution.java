f63efd62b7e9a79867df595de0271c0b5c3e5c91
yechan <yechan3110@gmail.com>
2023-08-02 23:58:45 +0900

    commit

--- Programmers/레벨0/외계어사전/Solution.java
+++ Programmers/레벨0/외계어사전/Solution.java
@@ -3,6 +3,24 @@ package Programmers.레벨0.외계어사전;
 public class Solution {
     public int solution(String[] spell, String[] dic) {
         int answer = 0;
+
+        for(int i = 0; i < dic.length; i++){
+            int count = dic[i].length();
+            int spcnt = count;
+
+            for(int j = 0; j < spell.length; j++){
+                if(!dic[i].contains(spell[j])){
+                    spcnt--;
+                }
+            }
+
+            if(count == spcnt){
+                return 1;
+            }
+        }
+
+        answer = 2;
+
         return answer;
     }
 }
