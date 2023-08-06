41983682730e2bd20e60b30a43acdf0ae677730d
yechan <yechan3110@gmail.com>
2023-08-06 21:58:22 +0900

    commit

---
+++ Programmers/레벨1/완주하지못한선수/Solution.java
@@ -0,0 +1,8 @@
+package Programmers.레벨1.완주하지못한선수;
+
+public class Solution {
+    public String solution(String[] participant, String[] completion) {
+        String answer = "";
+        return answer;
+    }
+}
