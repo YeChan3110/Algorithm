f478f6cdb26fcee77efd58e9edfb8b34eb815890
yechan <yechan3110@gmail.com>
2023-08-09 23:36:42 +0900

    commit

--- Programmers/레벨0/그림확대/Solution.java
+++ Programmers/레벨0/그림확대/Solution.java
@@ -2,7 +2,26 @@ package Programmers.레벨0.그림확대;
 
 public class Solution {
     public String[] solution(String[] picture, int k) {
-        String[] answer = {};
-        return answer;
+        int originalRows = picture.length;
+        int originalCols = picture[0].length();
+        int newRows = originalRows * k;
+        int newCols = originalCols * k;
+    
+        String[] enlargedPicture = new String[newRows];
+    
+        for (int i = 0; i < newRows; i++) {
+            StringBuilder row = new StringBuilder();
+            int originalRowIdx = i / k;
+    
+            for (int j = 0; j < newCols; j++) {
+                int originalColIdx = j / k;
+                row.append(picture[originalRowIdx].charAt(originalColIdx));
+            }
+    
+            enlargedPicture[i] = row.toString();
+        }
+    
+        return enlargedPicture;
     }
+    
 }
---
+++ Programmers/레벨1/체육복/Solution.java
@@ -0,0 +1,11 @@
+package Programmers.레벨1.체육복;
+
+public class Solution {
+    public int solution(int n, int[] lost, int[] reserve) {
+        int answer = 0;
+
+        
+
+        return answer;
+    }
+}
