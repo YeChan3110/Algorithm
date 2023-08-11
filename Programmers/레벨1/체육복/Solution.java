c8bbd0916074cb5a289aa9ff69043780dba81c97
yechan <yechan3110@gmail.com>
2023-08-11 23:43:10 +0900

    commit

--- Programmers/레벨1/체육복/Solution.java
+++ Programmers/레벨1/체육복/Solution.java
@@ -5,29 +5,30 @@ import java.util.*;
 public class Solution {
     public int solution(int n, int[] lost, int[] reserve) {
         int answer = 0;
+        Map<Integer,Integer> map = new HashMap<>();
 
         Arrays.sort(lost);
         Arrays.sort(reserve);
 
         for(int i = 0; i < lost.length; i++){
-            for(int j = 0; j < reserve.length; j++){
-                if(lost[i] == reserve[j]){
-                    answer++;
-                    lost[i] = -1;
-                    reserve[j] = -1;
-                    break;
-                }
-            }
+            map.put(lost[i], 0);
+        }
+
+        for(int i = 0; i < reserve.length; i++){
+            map.put(reserve[i],1);
         }
 
         for(int i = 0; i < lost.length; i++){
-            for(int j = 0; j < reserve.length; j++){
-                if((lost[i] == reserve[j]) || lost[i] + 1 == reserve[j]){
-                    answer++;
-                    reserve[j] = -1;
-                    break;
-                }
+            if(map.get(reserve[lost[i] - 1]) == 1){
+                map.put(lost[i], 1);   
             }
+        }
+
+        for(Map.Entry<Integer,Integer> entry : map.entrySet()){
+            int key = entry.getKey();
+            int value = entry.getValue();
+
+
         }
 
         return answer;
