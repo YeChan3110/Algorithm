4f147329afeea046dc4c30bb33823a2521082d66
yechan <yechan3110@gmail.com>
2023-08-14 23:43:30 +0900

    commit

--- Programmers/레벨0/수조작하기/Solution.java
+++ Programmers/레벨0/수조작하기/Solution.java
@@ -2,7 +2,17 @@ package Programmers.레벨0.수조작하기;
 
 public class Solution {
     public int solution(int n, String control) {
-        int answer = 0;
-        return answer;
+        for(char c : control.toCharArray()){
+            if(c == 'w') {
+                n += 1;
+            }else if(c == 's'){
+                n -= 1;
+            }else if(c == 'd'){
+                n += 10;
+            }else if(c == 'a'){
+                n -= 10;
+            }
+        }
+        return n;
     }
 }
