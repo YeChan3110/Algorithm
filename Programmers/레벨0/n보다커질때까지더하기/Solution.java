7439bdc1cf0eb883434944df91c6b43e8e9e4cd0
yechan <yechan3110@gmail.com>
2023-08-18 23:24:39 +0900

    commit

--- Programmers/레벨0/n보다커질때까지더하기/Solution.java
+++ Programmers/레벨0/n보다커질때까지더하기/Solution.java
@@ -3,6 +3,12 @@ package Programmers.레벨0.n보다커질때까지더하기;
 public class Solution {
     public int solution(int[] numbers, int n) {
         int answer = 0;
+
+        for(int i = 0; i < numbers.length; i++){
+            answer += numbers[i];
+            if(answer > n) break;
+        }
+
         return answer;
     }
 }
---
+++ Programmers/레벨0/이어붙인수/Solution.java
@@ -0,0 +1,19 @@
+package Programmers.레벨0.이어붙인수;
+
+public class Solution {
+    public int solution(int[] num_list) {
+        int answer = 0;
+        StringBuilder odd = new StringBuilder();
+        StringBuilder even = new StringBuilder();
+
+        for(int i = 0; i < num_list.length; i++){
+            if(num_list[i] % 2 == 0){
+                even.append(num_list[i]);
+            }else{
+                odd.append(num_list[i]);
+            }
+        }
+        answer = Integer.parseInt(odd.toString()) + Integer.parseInt(even.toString());
+        return answer;
+    }
+}
