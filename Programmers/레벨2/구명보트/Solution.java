f9e1861afe59198b8c78c5946460895ca7ab9322
yechan <yechan3110@gmail.com>
2023-08-21 23:56:34 +0900

    commit

--- Programmers/레벨2/구명보트/Solution.java
+++ Programmers/레벨2/구명보트/Solution.java
@@ -7,17 +7,12 @@ public class Solution {
         int answer = 0;
 
         Arrays.sort(people);
-
-        ArrayList<Integer> list = new ArrayList<Integer>();
-        for(int weight : people){
-            list.add(weight);
-        }
         
         int left = 0;
-        int right = list.size() - 1;
+        int right = people.length - 1;
 
         while(left <= right) {
-            if(list.get(left) + list.get(right) <= limit) {
+            if(people[left] + people[right] <= limit) {
                 left++;
             }
             right--;
