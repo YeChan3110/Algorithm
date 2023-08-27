494e99274f7db1bec14d5e359313fbb6d1edadf9
yechan <yechan3110@gmail.com>
2023-08-27 22:04:57 +0900

    commit

---
+++ 백준/별찍기/별찍기7.java
@@ -0,0 +1,30 @@
+package 백준.별찍기;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class 별찍기7 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int count = Integer.parseInt(br.readLine());
+
+        for(int i = 0; i < count; i++){
+            StringBuilder sb = new StringBuilder();
+            for(int j = 0; j < i; j++){
+                sb.append(" ");
+            }
+            for(int j = i; j < count; j++){
+                sb.append("*");
+            }
+            bw.write(sb.toString() + "\n");
+        }
+
+        bw.flush();
+    }
+}
+
+
