2554fb8da0bc4ae0b4ac917d4ad374f15c6130c2
yechan <yechan3110@gmail.com>
2023-09-14 23:47:16 +0900

    commit

--- Programmers/레벨2/n개의최소공배수/Solution.java
+++ Programmers/레벨2/n개의최소공배수/Solution.java
@@ -3,19 +3,19 @@ package Programmers.레벨2.n개의최소공배수;
 public class Solution {
     public int solution(int[] arr) {
         int answer = 0;
-        int gcd = GCD(arr[0], arr[1]); 
+        int gcd = gcd(arr[0], arr[1]); 
         int lcm = arr[0] * arr[1] / gcd; 
         
         for(int i = 2; i < arr.length; i++){
-            gcd = GCD(lcm, arr[i]);
+            gcd = gcd(lcm, arr[i]);
             lcm = lcm * arr[i] / gcd;
         }
         answer = lcm;
         return answer;
     }
     
-    private int GCD(int a, int b){
+    private int gcd(int a, int b){
         if( a % b == 0) return b;
-        return GCD(b, a % b);
+        return gcd(b, a % b);
     }
 }
