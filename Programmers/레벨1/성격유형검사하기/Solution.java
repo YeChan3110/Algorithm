bc92b58f6f914ad4b3f4f62e686e234cfe60ff88
yechan <yechan3110@gmail.com>
2023-09-19 21:24:19 +0900

    commit

---
+++ Programmers/레벨1/성격유형검사하기/Solution.java
@@ -0,0 +1,8 @@
+package Programmers.레벨1.성격유형검사하기;
+
+public class Solution {
+    public String solution(String[] survey, int[] choices) {
+        String answer = "";
+        return answer;
+    }
+}
