304d3670495b8b72504db92a93c0ae984dfe1241
yechan <yechan3110@gmail.com>
2023-09-28 23:24:41 +0900

    commit

--- Programmers/레벨0/문자열정수의합/Solution.java
+++ Programmers/레벨0/문자열정수의합/Solution.java
@@ -3,6 +3,11 @@ package Programmers.레벨0.문자열정수의합;
 public class Solution {
     public int solution(String num_str) {
         int answer = 0;
+
+        for(char c : num_str.toCharArray()){
+            
+        }
+
         return answer;
     }
 }
